c441b3d57fc7db764cbab379c70a2099dca4a327
harunazakaria <Harunzy55@gmail.com>
2022-08-23 20:04:53 -0700

    add to solution

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -10,16 +10,4 @@ def magic_calculation(a, b, c):
 
         return (a + b)
 
-    return (a*b - c)i#!/usr/bin/python3
-
-def magic_calculation(a, b, c):
-
-    if a < b:
-
-        return (c)
-
-    if c > b:
-
-        return (a + b)
-
     return (a*b - c)
