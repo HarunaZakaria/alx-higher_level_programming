eb870e2e110936bcce7ebe477644f61ce9ba60e3
harunazakaria <Harunzy55@gmail.com>
2022-08-28 12:38:24 -0700

    add to solution

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,32 +1,63 @@
-#!/usr/bin/py3
+#!/usr/bin/python3
+
 from sys import argv
+
 from calculator_1 import add, sub, mul, div
 
+
+
 if __name__ == "__main__":
+
     args = len(argv)
 
-     if args != 4:
-         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-          exit(1)
 
-     a = int(argv[1])
-     op = argv[2]
-     b = int(argv[3])
 
-     if op == "+":
-         num = add(a, b)
-     
-     elif op == "-":
-         num = sub(a, b)
+    if args != 4:
 
-      elif op == "*":
-          num = mul(a, b)
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
 
-     elif op == "/":
-         num = div(a, b)
+        exit(1)
 
-     else:
-         print("Unknown operator. Available operators: +, -, * and /")
-         exit(1)
+
+
+    a = int(argv[1])
+
+    op = argv[2]
+
+    b = int(argv[3])
+
+
+
+    if op == "+":
+
+        num = add(a, b)
+
+
+
+    elif op == "-":
+
+        num = sub(a, b)
+
+
+
+    elif op == "*":
+
+        num = mul(a, b)
+
+
+
+    elif op == "/":
+
+        num = div(a, b)
+
+
+
+    else:
+
+        print("Unknown operator. Available operators: +, -, * and /")
+
+        exit(1)
+
+
 
     print("{:d} {} {:d} = {:d}".format(a, op, b, num))
