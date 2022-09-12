647f9f310a799f02e974096a32d3bfa5152a44e2
harunazakaria <Harunzy55@gmail.com>
2022-09-12 16:46:33 -0700

    add to solution

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -6,7 +6,7 @@ void print_python_float(PyObject *p);
 
 /**
  * print_python_list - Prints basic info about Python lists.
- * @p: A PyObject, list object.
+ * @p: A PyObject list object.
  */
 void print_python_list(PyObject *p)
 {
@@ -21,7 +21,6 @@ void print_python_list(PyObject *p)
 	fflush(stdout);
 
 	printf("[*] Python list info\n");
-
 	if (strcmp(p->ob_type->tp_name, "list") != 0)
 	{
 		printf("  [ERROR] Invalid List Object\n");
@@ -35,7 +34,7 @@ void print_python_list(PyObject *p)
 	{
 		type = list->ob_item[i]->ob_type->tp_name;
 		printf("Element %ld: %s\n", i, type);
-		if (strcmp(type, "bytes") == 0);
+		if (strcmp(type, "bytes") == 0)
 			print_python_bytes(list->ob_item[i]);
 		else if (strcmp(type, "float") == 0)
 			print_python_float(list->ob_item[i]);
